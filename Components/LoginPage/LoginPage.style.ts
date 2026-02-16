38173d54bd4a53829eef972263942a849f8aa407
ohsungjun <sungjun12110@gmail.com>
2026-02-17 00:02:39 +0900

    style: 로그인 화면 입력창 및 버튼 중앙 정렬 최적화

--- Components/LoginPage/LoginPage.style.ts
+++ Components/LoginPage/LoginPage.style.ts
@@ -1,16 +1,10 @@
-//반응형 유틸리티(wp, hp)**와 **중앙 정렬 레이아웃(contentWrapper)**이 아주 잘 구현
 import { StyleSheet, Dimensions, Platform } from 'react-native';
 
 const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get('window');
 
-// 📱 피그마 디자인 기준 사이즈 (iPhone 14/15)
 const DESIGN_WIDTH = 393;
 const DESIGN_HEIGHT = 852;
 
-/**
- * 📏 반응형 비율 계산 함수
- * 너비(wp)와 높이(hp)를 각각 계산하여 기기별 최적의 위치를 잡습니다.
- */
 const wp = (size: number) => (size / DESIGN_WIDTH) * SCREEN_WIDTH;
 const hp = (size: number) => (size / DESIGN_HEIGHT) * SCREEN_HEIGHT;
 
@@ -20,17 +14,16 @@ export default StyleSheet.create({
     backgroundColor: '#FFFFFF',
   },
   
-  // 1. 상태바 영역 (iOS 노치 대응)
   statusBarSpace: {
     height: Platform.OS === 'ios' ? hp(44) : 0,
   },
 
-  // 2. 로고 박스 (image 1)
+  // 2. 로고 박스 (중앙 정렬로 수정)
   logoBox: {
     position: 'absolute',
     width: wp(244),
     height: hp(87),
-    left: wp(75),
+    alignSelf: 'center', // 👈 left 대신 alignSelf 사용으로 완벽한 중앙 정렬
     top: hp(237),
   },
   logoImage: {
@@ -38,12 +31,12 @@ export default StyleSheet.create({
     height: '100%',
   },
 
-  // 3. 이메일 입력창 (Rectangle 444)
+  // 3. 이메일 입력창 (중앙 정렬로 수정)
   inputEmail: {
     position: 'absolute',
     width: wp(320),
     height: hp(48),
-    left: wp(27),
+    alignSelf: 'center', // 👈 중앙 정렬
     top: hp(377),
     backgroundColor: '#FFFFFF',
     borderWidth: 1,
@@ -55,12 +48,12 @@ export default StyleSheet.create({
     color: '#000',
   },
 
-  // 4. 비밀번호 입력창 (Rectangle 443)
+  // 4. 비밀번호 입력창 (중앙 정렬로 수정)
   inputPassword: {
     position: 'absolute',
     width: wp(320),
     height: hp(48),
-    left: wp(27),
+    alignSelf: 'center', // 👈 중앙 정렬
     top: hp(435),
     backgroundColor: '#FFFFFF',
     borderWidth: 1,
@@ -72,12 +65,12 @@ export default StyleSheet.create({
     color: '#000',
   },
 
-  // 5. 로그인 버튼 (Rectangle 442)
+  // 5. 로그인 버튼 (중앙 정렬로 수정)
   loginButton: {
     position: 'absolute',
     width: wp(320),
     height: hp(48),
-    left: wp(27),
+    alignSelf: 'center', // 👈 중앙 정렬
     top: hp(506),
     backgroundColor: '#0068FF',
     borderWidth: 1,
@@ -93,12 +86,12 @@ export default StyleSheet.create({
     fontFamily: 'Inter',
   },
 
-  // 6. 하단 메뉴 컨테이너 (Group 344)
+  // 6. 하단 메뉴 컨테이너
   bottomMenuContainer: {
     position: 'absolute',
-    width: wp(320), // 중앙 정렬을 위해 너비 확보
+    width: wp(320),
     height: hp(22),
-    left: wp(36.5), // (393 - 320) / 2
+    alignSelf: 'center', // 👈 일관성 있게 alignSelf로 변경
     top: hp(664),
     flexDirection: 'row',
     justifyContent: 'center',
