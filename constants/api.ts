013c29f70abf2513b2baea37855ea9af7696cef4
ohsungjun <sungjun12110@gmail.com>
2026-04-06 17:56:22 +0900

    방명록기능 추가해야함,조회,작성,삭제 나머지는 됨 공유페이지

--- constants/api.ts
+++ constants/api.ts
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 // ⚠️ ngrok 주소 바뀌면 여기만 수정
 // 각자 본인 ngrok URL로 변경 후 사용
-export const API_BASE_URL = 'https://0ce8-59-18-155-76.ngrok-free.app';
+export const API_BASE_URL = 'https://099d-220-66-64-196.ngrok-free.app';
 
 axios.defaults.headers.common['ngrok-skip-browser-warning'] = 'true';
